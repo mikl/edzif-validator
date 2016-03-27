e6614dbe50346e1102be881b1dcb9f0555fe6325
Mikkel Hoegh <mikkel@hoegh.org>
2016-03-27 20:55:48 +0200

    Added test for new GTLDs

--- test/basic_zone_validation.js
+++ test/basic_zone_validation.js
@@ -7,9 +7,19 @@ const validator = require('..');
 
 lab.experiment('basic zone with default options', () => {
   lab.test('simplest use case', (done) => {
-    const basic_zone = require('./fixtures/basic_zone')
+    const zone = require('./fixtures/zone_basic');
 
-    const result = validator(basic_zone);
+    const result = validator(zone);
+
+    Code.expect(result.valid).to.be.a.boolean();
+    Code.expect(result.valid).to.be.true();
+    done();
+  });
+
+  lab.test('new GTLD zone name', (done) => {
+    const zone = require('./fixtures/zone_basic_new_gtld');
+
+    const result = validator(zone);
 
     Code.expect(result.valid).to.be.a.boolean();
     Code.expect(result.valid).to.be.true();
