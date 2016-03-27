38dc444fab4722a0c7c416565367768ba1d870b1
Mikkel Hoegh <mikkel@hoegh.org>
2016-03-28 01:20:06 +0200

    Simplified tests to return promise
    Thanks to @gergoerdosi for the tip:
    https://github.com/hapijs/lab/issues/553#issuecomment-202167919

--- test/basic_zone_validation.js
+++ test/basic_zone_validation.js
@@ -9,31 +9,28 @@ lab.experiment('basic zone with default options', () => {
   lab.test('simplest use case', (done) => {
     const zone = require('./fixtures/zone_basic');
 
-    validate(zone).then((result) => {
+    return validate(zone).then((result) => {
       Code.expect(result.valid).to.be.a.boolean();
       Code.expect(result.valid).to.be.true();
-      done();
     });
   });
 
-  lab.test('new GTLD zone name', (done) => {
+  lab.test('new GTLD zone name', () => {
     const zone = require('./fixtures/zone_basic_new_gtld');
 
-    validate(zone).then((result) => {
+    return validate(zone).then((result) => {
       Code.expect(result.valid).to.be.a.boolean();
       Code.expect(result.valid).to.be.true();
-      done();
     });
   });
 
-  lab.test('zone without name property', (done) => {
+  lab.test('zone without name property', () => {
     const zone = require('./fixtures/zone_no_name');
 
-    validate(zone).then((result) => {
+    return validate(zone).then((result) => {
       Code.expect(result.valid).to.be.a.boolean();
       Code.expect(result.valid).to.be.false();
       Code.expect(result.errors).to.be.an.object();
-      done();
     });
   });
 });
