d33efd6901b5a54c10da2630bb5fda11ae3391bf
Mikkel Hoegh <mikkel@hoegh.org>
2016-04-03 13:04:23 +0200

    Whitelist for characters in TXT records

--- src/validators/record_type_txt.js
+++ src/validators/record_type_txt.js
@@ -6,5 +6,9 @@ const recordBase = require('./record_base');
 
 module.exports = Hoek.clone(recordBase).keys({
   record_type: Joi.string().valid('TXT').required(),
-  text_content: Joi.string().required()
+  // Allowed in TXT records are ASCII letters, plus a selected set of
+  // symbols, which translates to this gobbledygook you see in the regex
+  // below. The \[\\\]\ sequence just whitelists the characters [\], but
+  // since they all need escaping in regex, it becomes a bit strange.
+  text_content: Joi.string().required().regex(/^[A-Za-z0-9 !"#\$%&'()*+,-.\/:;<=>?@\[\\\]\^_`{\|}~-]+$/)
 });
