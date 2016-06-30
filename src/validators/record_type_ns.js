496f7f63938f050f638d9a2aec216add256bba15
Mikkel Hoegh <mikkel@hoegh.org>
2016-06-30 22:17:36 +0200

    NS record type validation

---
+++ src/validators/record_type_ns.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const Hoek = require('hoek');
+const Joi = require('joi');
+const domainName = require('./domain_name');
+const recordBase = require('./record_base');
+
+module.exports = Hoek.clone(recordBase).keys({
+  record_type: Joi.string().valid('NS').required(),
+  name: domainName.required()
+});
