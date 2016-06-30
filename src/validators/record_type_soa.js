ec3c871bb49f37e4310a07cfc0348d18b1198035
Mikkel Hoegh <mikkel@hoegh.org>
2016-06-30 22:13:04 +0200

    Implemented SOA record validation

---
+++ src/validators/record_type_soa.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const Hoek = require('hoek');
+const Joi = require('joi');
+const domainName = require('./domain_name');
+
+// @see https://blog.bobcares.com/understanding-soa-records/
+// @see http://rscott.org/dns/soa.html
+
+module.exports = Joi.object({
+  name: domainName.required(),
+  record_type: Joi.string().valid('SOA').required(),
+  ttl: Joi.number().integer().positive(),
+  primary_server: domainName.required(),
+  responsible_person: domainName.required(),
+  serial: Joi.number().integer().positive().required(),
+  refresh: Joi.number().integer().positive().required(),
+  retry: Joi.number().integer().positive().required(),
+  expire: Joi.number().integer().positive().required(),
+  minimum_ttl: Joi.number().integer().positive().required(),
+});
